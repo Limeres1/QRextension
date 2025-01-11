6620b4d0aae757b22793767a8b11863d382d00ae
Limeres <joacolimeres@gmail.com>
2025-01-11 11:12:49 -0300

    .

--- scripts/popup.js
+++ scripts/popup.js
@@ -15,33 +15,6 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     }
 });
 
-
-/*
-button.addEventListener("click", () => {
-    if (currentUrl) {
-        QRCode.toDataURL(currentUrl)
-            .then((dataUrl) => {
-                image.src = dataUrl; // Asignar la URL de la imagen al atributo src
-                console.log("QR Code generado exitosamente.");
-            })
-            .catch((error) => {
-                console.error("Error generando el QR Code:", error);
-            });
-    } else {
-        console.error("No hay URL disponible para generar el QR Code.");
-    }
-});
-*/
-
-const qrcode = new QRCode(document.getElementById('qrcode'), {
-    text: 'https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs/qrcode.min.js',
-    width: 128,
-    height: 128,
-    colorDark: '#000',
-    colorLight: '#fff',
-    correctLevel: QRCode.CorrectLevel.H
-});
-
 copyButton.addEventListener("click", () => {
     const textToCopy = currentUrlElement.textContent; //Obtiene el elemento del tag <a>
 
