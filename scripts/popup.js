8bc8bedb1e8e0b08c8ec48bb2964a349ab31bb18
Limeres <joacolimeres@gmail.com>
2025-01-13 16:16:45 -0300

    .

--- scripts/popup.js
+++ scripts/popup.js
@@ -32,7 +32,7 @@ copyButton.addEventListener("click", () => {
 
 button.addEventListener("click", () => {
     const textToCopy = currentUrlElement.textContent;
-    QrCode.toDataURL(textToCopy)
+    QRCode.toDataURL(textToCopy)
         .then((dataUrl) => {
             image.src = dataUrl;
         })
